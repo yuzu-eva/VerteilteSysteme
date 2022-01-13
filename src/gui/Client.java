c1219bec3800bd4de0082666f0f831e5f9d5a78c
cafebabe <77344710+yuzu-eva@users.noreply.github.com>
2022-01-13 18:25:56 +0100

    added chat history
    implemented chat history. sent messages will be saved by the server in their encrypted state. the history is saved into a txt-file, which is read and decrypted by clients on start-up.

--- src/gui/Client.java
+++ src/gui/Client.java
@@ -1,6 +1,5 @@
 package gui;
 
-import javax.crypto.SecretKey;
 import javax.print.Doc;
 import javax.swing.*;
 import javax.swing.text.BadLocationException;
@@ -16,6 +15,7 @@ import java.nio.charset.StandardCharsets;
 import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -23,21 +23,17 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.*;
 
-import gui.AESEncryptor;
-
 public class Client {
 
-	//Alle Elemente der GUI werden erstellt
+	// Alle Elemente der GUI werden erstellt
 	JFrame clientFrame;
 	JPanel clientPanel;
 	JTextArea textPane = new JTextArea();
-	
-	//textPane.setText("");
-	//StyledDocument doc = textPane.getDocument();
-	//SimpleAttributeSet keyWord = new SimpleAttributeSet();
-	
-	
-	
+
+	// textPane.setText("");
+	// StyledDocument doc = textPane.getDocument();
+	// SimpleAttributeSet keyWord = new SimpleAttributeSet();
+
 	JTextField textField_ClientMessages;
 	JButton button_SendMessage;
 	JButton button_emoji_laugh;
@@ -46,81 +42,75 @@ public class Client {
 	JButton button_emoji_rock;
 	JButton button_emoji_plane;
 	JTextField textField_Username;
-	
-	
-		
+
 	Socket client;
 	PrintWriter writer;
 	BufferedReader reader;
 	Date date;
-	
+
 	// Key und IV zum Ver-/Entschluesseln der Nachrichten.
 	private static final String secretKey = "GqHUYGzzgVeQezZZEPJwSw==";
 	private static final String initialValue = "ImLWb42jXO8VnvkQ";
-	
-	
+
 	public static void main(String[] args) {
-		
-		//Neue Instanz der Klasse Client wird erstellt, f�r die eine GUI erstellt wird
+
+		// Neue Instanz der Klasse Client wird erstellt, f�r die eine GUI erstellt wird
 		Client c = new Client();
 		c.createGUI();
-		
-		
-		
-				
+
 	}
-	
-	//Erstellung der GUI
+
+	// Erstellung der GUI
 	public void createGUI() {
 		clientFrame = new JFrame("Chatprogramm");
 		clientFrame.setSize(800, 600);
-		
+
 		clientPanel = new JPanel();
-		
+
 		textPane = new JTextArea();
-		//textPane.setEditable(false);
+		// textPane.setEditable(false);
 		textPane.setFont(new Font("SansSerif", Font.PLAIN, 12));
-		
+
 		textField_ClientMessages = new JTextField(38);
 		textField_ClientMessages.addKeyListener(new SendPressEnterListener());
 		textField_ClientMessages.setFont(new Font("SansSerif", Font.PLAIN, 12));
-		
+
 		button_SendMessage = new JButton("Senden");
 		button_SendMessage.addActionListener(new SendButtonListener());
-		
+
 		button_emoji_laugh = new JButton("\uD83D\uDE02");
 		button_emoji_laugh.addActionListener(new Klick_Emoji1_Listener());
-		
+
 		button_emoji_smile = new JButton("\uD83D\uDE00");
 		button_emoji_smile.addActionListener(new Klick_Emoji2_Listener());
-		
+
 		button_emoji_rainbow = new JButton("\uD83C\uDF08");
 		button_emoji_rainbow.addActionListener(new Klick_Emoji3_Listener());
-		
+
 		button_emoji_rock = new JButton("\uD83E\uDD18");
 		button_emoji_rock.addActionListener(new Klick_Emoji4_Listener());
-		
+
 		button_emoji_plane = new JButton("\u2708");
 		button_emoji_plane.addActionListener(new Klick_Emoji5_Listener());
-		
+
 		textField_Username = new JTextField(10);
 		textField_Username.setFont(new Font("SansSerif", Font.PLAIN, 10));
-		
+
 		JScrollPane scrollPane_Messages = new JScrollPane(textPane);
 		scrollPane_Messages.setPreferredSize(new Dimension(700, 500));
 		scrollPane_Messages.setMinimumSize(new Dimension(700, 500));
 		scrollPane_Messages.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		scrollPane_Messages.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
-		
-		if(!connectToServer()) {
-			//Evtl. Connect-Label in GUI und Console 
+
+		if (!connectToServer()) {
+			// Evtl. Connect-Label in GUI und Console
 		}
-		
-		//Tread wird erstellt
-		Thread t = new Thread(new MassegesFromServerListener());
+
+		// Tread wird erstellt
+		Thread t = new Thread(new MassagesFromServerListener());
 		t.start();
-		
-		//GUI-Elemete werden in clientPanel zusammengefasst
+
+		// GUI-Elemete werden in clientPanel zusammengefasst
 		clientPanel.add(scrollPane_Messages);
 		clientPanel.add(textField_Username);
 		clientPanel.add(textField_ClientMessages);
@@ -130,112 +120,120 @@ public class Client {
 		clientPanel.add(button_emoji_rainbow);
 		clientPanel.add(button_emoji_rock);
 		clientPanel.add(button_emoji_plane);
-		
-		
-		//Es wird ein BorderLayout hinzugef�gt
+
+		// Es wird ein BorderLayout hinzugef�gt
 		clientFrame.getContentPane().add(BorderLayout.CENTER, clientPanel);
-		
-		//Erlaubt das Schli�en der Applikation durch das rote X
+
+		// Erlaubt das Schli�en der Applikation durch das rote X
 		clientFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		clientFrame.setVisible(true);
-		
-		
-		
-		
+
 	}
-	
-	//Client im locelhost verbindet sich mit dem Server
+
+	// Client im localhost verbindet sich mit dem Server
 	public boolean connectToServer() {
 		try {
 			client = new Socket(InetAddress.getLocalHost(), 5555);
 			reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
 			writer = new PrintWriter(client.getOutputStream());
 			appendTextMessages("Netzwerkverbindung hergestellt");
-			
+			retrieveChatHistory();
+
 			return true;
 		} catch (Exception e) {
-			appendTextMessages ("Netzwerkverbindung nicht m�glich");
+			appendTextMessages("Netzwerkverbindung nicht m�glich");
 			e.printStackTrace();
-			
+
 			return false;
 		}
 	}
 	
+	// Methode zum Aufrufen des Chatverlaufs.
+	public void retrieveChatHistory() throws Exception {
+		AESEncryptor aes = new AESEncryptor();
+		aes.initFromStrings(secretKey, initialValue);
+
+		File file = new File("chatHistory.txt");
+		Scanner scan = new Scanner(file);
+
+		while (scan.hasNextLine()) {
+			String decryptedHistory = aes.decrypt(scan.nextLine());
+			appendTextMessages(decryptedHistory);
+		}
+
+		scan.close();
+
+	}
+
 	public void sendMessageToServer() throws Exception {
-		
+
 		// Encryptor-Object initialisieren
 		AESEncryptor aes = new AESEncryptor();
 		aes.initFromStrings(secretKey, initialValue);
-		
-		//Zeitstempel wird erstellt
+
+		// Zeitstempel wird erstellt
 		SimpleDateFormat date = new SimpleDateFormat("HH:mm");
 		String timeStamp = date.format(new Date());
-			
-		//Bugfix: Neue Zeile; neue Nachricht		
-		//System.out.println(textField_ClientMessages);
-		
-		//Nachricht wird mit AES/GCM-Algorithmus verschluesselt und gesendet
-		String encryptedMessage = aes.encrypt(timeStamp + " Uhr | " + textField_Username.getText() + ": " + textField_ClientMessages.getText());
+
+		// Bugfix: Neue Zeile; neue Nachricht
+		// System.out.println(textField_ClientMessages);
+
+		// Nachricht wird mit AES/GCM-Algorithmus verschluesselt und gesendet
+		String encryptedMessage = aes.encrypt(
+				timeStamp + " Uhr | " + textField_Username.getText() + ": " + textField_ClientMessages.getText());
 		writer.println(encryptedMessage);
 		writer.flush();
-		
-		//Eingabefeld wird wieder leer und der Client kann direkt weiterschreiben.
+
+		// Eingabefeld wird wieder leer und der Client kann direkt weiterschreiben.
 		textField_ClientMessages.setText("");
 		textField_ClientMessages.requestFocus();
-		
-		
-		
-	
+
 	}
-	
+
 	public void appendTextMessages(String message) {
-		//Nachricht wird angezeigt
+		// Nachricht wird angezeigt
 		textPane.append(message + "\n");
-		//try {
-		      //Document doc = 
-			
-		      //doc.insertString(doc.getLength(), message, null);
-		   //} catch(BadLocationException exc) {
-		     // exc.printStackTrace();
-		  // }
-		//textPane.setText(message + "\n");
-		
+		// try {
+		// Document doc =
+
+		// doc.insertString(doc.getLength(), message, null);
+		// } catch(BadLocationException exc) {
+		// exc.printStackTrace();
+		// }
+		// textPane.setText(message + "\n");
+
 	}
-	
-	//Nachricht wird durch das Dr�cken von Enter versendet
+
+	// Nachricht wird durch das Dr�cken von Enter versendet
 	public class SendPressEnterListener implements KeyListener {
-		
+
 		@Override
 		public void keyPressed(KeyEvent arg0) {
-			if(arg0.getKeyCode() == KeyEvent.VK_ENTER) {
+			if (arg0.getKeyCode() == KeyEvent.VK_ENTER) {
 				try {
 					sendMessageToServer();
 				} catch (Exception e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
-				 
-				  
+
 			}
 		}
-		
+
 		@Override
 		public void keyReleased(KeyEvent arg0) {
-			
-			
+
 		}
-		
+
 		@Override
-		public void keyTyped(KeyEvent arg0) {}
-		
-		
-		
+		public void keyTyped(KeyEvent arg0) {
+		}
+
 	}
-	
-	//Nachricht wird durch das Dr�cken des Buttons "Senden" verschickt
+
+	// Nachricht wird durch das Dr�cken des Buttons "Senden" verschickt
 	public class SendButtonListener implements ActionListener {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			try {
@@ -244,81 +242,78 @@ public class Client {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-			
-			 
-			    
+
 		}
 	}
-	
-	//Emoji wird bei Klick des Buttons eingef�gt
+
+	// Emoji wird bei Klick des Buttons eingef�gt
 	public class Klick_Emoji1_Listener implements ActionListener {
-		
-			
+
 		@Override
 		public void actionPerformed(ActionEvent e1) {
 			textField_ClientMessages.setText("\uD83D\uDE02");
-			//textField_ClientMessages.setText("\uD83D\uDE02");
-			//sendMessageToServer();
+			// textField_ClientMessages.setText("\uD83D\uDE02");
+			// sendMessageToServer();
 		}
 	}
-	//Emoji wird bei Klick des Buttons eingef�gt
+
+	// Emoji wird bei Klick des Buttons eingef�gt
 	public class Klick_Emoji2_Listener implements ActionListener {
-		
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e1) {
 			textField_ClientMessages.setText("\uD83D\uDE00");
-			//sendMessageToServer();
+			// sendMessageToServer();
 		}
 	}
-	//Emoji wird bei Klick des Buttons eingef�gt
+
+	// Emoji wird bei Klick des Buttons eingef�gt
 	public class Klick_Emoji3_Listener implements ActionListener {
-		
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e1) {
 			textField_ClientMessages.setText("\uD83C\uDF08");
-			//sendMessageToServer();
+			// sendMessageToServer();
 		}
 	}
-	//Emoji wird bei Klick des Buttons eingef�gt
+
+	// Emoji wird bei Klick des Buttons eingef�gt
 	public class Klick_Emoji4_Listener implements ActionListener {
-		
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e1) {
 			textField_ClientMessages.setText("\uD83E\uDD18");
-			//sendMessageToServer();
+			// sendMessageToServer();
 		}
 	}
-	//Emoji wird bei Klick des Buttons eingef�gt
+
+	// Emoji wird bei Klick des Buttons eingef�gt
 	public class Klick_Emoji5_Listener implements ActionListener {
-		
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e1) {
 			textField_ClientMessages.setText("\u2708");
-			//sendMessageToServer();
+			// sendMessageToServer();
 		}
 	}
-	
-	//Nachrichten werden so lange empfangen, wie noch Nachrichten gesendet werden
-	public class MassegesFromServerListener implements Runnable {
-		
+
+	// Nachrichten werden so lange empfangen, wie noch Nachrichten gesendet werden
+	public class MassagesFromServerListener implements Runnable {
+
 		@Override
 		public void run() {
 			String message;
-			
+
 			// Encryptor-Object initialisieren
 			AESEncryptor aes = new AESEncryptor();
 			aes.initFromStrings(secretKey, initialValue);
-			
+
 			try {
-				while((message = reader.readLine()) != null) {
+				while ((message = reader.readLine()) != null) {
 					// Die vom Server empfangene Nachricht wird entschluesselt und ausgegeben.
 					String decryptedMessage = aes.decrypt(message);
 					appendTextMessages(decryptedMessage);
-					
+
 				}
 			} catch (Exception e) {
 				appendTextMessages("Nachricht konnte nicht empfangen werden");
@@ -326,7 +321,5 @@ public class Client {
 			}
 		}
 	}
-	
-	
-	
+
 }
--- src/gui/Server.java
+++ src/gui/Server.java
@@ -6,151 +6,145 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
-
 public class Server {
-	
-	
-	
-	//ServerSocket und PrintWriter werden erstellt
+
+	// ServerSocket und PrintWriter werden erstellt
 	ServerSocket server;
 	ArrayList<PrintWriter> list_clientWriter;
-	
-	//Level werden erstellt f�r die Anzeige von Nachrichten ind er Konsole
+
+	// Level werden erstellt f�r die Anzeige von Nachrichten ind er Konsole
 	final int LEVEL_ERROR = 1;
 	final int LEVEL_NORMAL = 0;
-	
+
 	public static void main(String[] args) {
-		
-		//Neue Instanz des Servers wird erzeugt
-		//Solange der Server l�uft, h�rt er den Clients zu und �bertr�gt Nachrichten
+
+		// Neue Instanz des Servers wird erzeugt
+		// Solange der Server l�uft, h�rt er den Clients zu und �bertr�gt Nachrichten
 		Server s = new Server();
 		if (s.runServer()) {
 			s.listenToClients();
-		}else {
-			//Do nothing
+		} else {
+			// Do nothing
 		}
-		
+
 	}
-	
-	//Holt sich den InputStream des Clients
+
+	// Holt sich den InputStream des Clients
 	public class ClientHandler implements Runnable {
-		
+
 		Socket client;
 		BufferedReader reader;
-		
+
 		public ClientHandler(Socket client) {
 			try {
 				this.client = client;
 				reader = new BufferedReader(new InputStreamReader(client.getInputStream()));
-			}catch(IOException e) {
-				e.printStackTrace();
-				
-			}
-			
-		}
-		
-		//Solange Nachrichten von Clients gesendet werden, gibt der Server sie weiter.
-		//Server gibt Nachrichten normal auf der Console aus und auch in der GUI
-		@Override
-		public void run() {
-			
-			String nachricht;
-			
-			try {
-				
-				while((nachricht = reader.readLine()) != null) {
-					appendTextToConsole("Vom Client: \n" + nachricht, LEVEL_NORMAL);
-					sendToAllClients(nachricht);		
-				}
-				
 			} catch (IOException e) {
 				e.printStackTrace();
-				
+
+			}
+
+		}
+
+		// Solange Nachrichten von Clients gesendet werden, gibt der Server sie weiter.
+		// Server gibt Nachrichten normal auf der Console aus und auch in der GUI.
+		// Alle Nachrichten werden zudem noch in einer Textdatei gespeichert, um den 
+		// Verlauf beim spaeteren starten des Clients  erneut aufzurufen.
+		@Override
+		public void run() {
+
+			String nachricht;
+
+			try {
+
+				while ((nachricht = reader.readLine()) != null) {
+					appendTextToConsole("Vom Client: \n" + nachricht, LEVEL_NORMAL);
+					saveChatHistory(nachricht);
+					sendToAllClients(nachricht);
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		}
-		
-		
+
 	}
-	//Der Server nimmt alle Client-Verbindungen an, holt sich den OUtputStream und er�ffnet einen Thread
-	
+	// Der Server nimmt alle Client-Verbindungen an, holt sich den OutputStream und
+	// er�ffnet einen Thread
+
 	public void listenToClients() {
-		while(true) {
+		while (true) {
 			try {
 				Socket client = server.accept();
-				
+
 				PrintWriter writer = new PrintWriter(client.getOutputStream());
 				list_clientWriter.add(writer);
-				
-				
-			
-				
-				
-				
-				
+
 				Thread clientThread = new Thread(new ClientHandler(client));
 				clientThread.start();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
+
 		}
 	}
-	
-	//Wenn der Server gestartet wird, zeigt die Konsole dies im Level Error
-	//Sollte der Server nicht gestartet werden k�nnen, wird die andere Nachricht ausgegeben
+
+	// Wenn der Server gestartet wird, zeigt die Konsole dies im Level Error
+	// Sollte der Server nicht gestartet werden k�nnen, wird die andere Nachricht
+	// ausgegeben
 	public boolean runServer() {
 		try {
-		server = new ServerSocket(5555);
-		appendTextToConsole("Server wurde gestartet", LEVEL_ERROR);
-		
-		list_clientWriter = new ArrayList<PrintWriter>();
-		return true;
+			server = new ServerSocket(5555);
+			appendTextToConsole("Server wurde gestartet", LEVEL_ERROR);
+
+			list_clientWriter = new ArrayList<PrintWriter>();
+			return true;
 		} catch (IOException e) {
 			appendTextToConsole("Server konnte nicht gestartet werden!", LEVEL_ERROR);
 			e.printStackTrace();
 			return false;
 		}
-		
-		
+
 	}
-	
-	//Text wird auf der Konsoke ausgegeben
+
+	// Text wird auf der Konsole ausgegeben
 	public void appendTextToConsole(String message, int level) {
-		if(level == LEVEL_ERROR) {
+		if (level == LEVEL_ERROR) {
 			System.err.println(message + "\n");
-		}else {
+		} else {
 			System.out.println(message + "\n");
 		}
-		
+
 	}
-	
-	//Der Iterator z�hlt die Nachrichten und solange noch eine Nachricht gesendet wird,
-	// wird diese an alle Clients gecshickt
+
+	// Der Iterator z�hlt die Nachrichten und solange noch eine Nachricht gesendet
+	// wird, wird diese an alle Clients geschickt.
 	public void sendToAllClients(String message) throws IOException {
-		Iterator it = list_clientWriter.iterator();
-		
-		while(it.hasNext()) {
-			PrintWriter writer = (PrintWriter) it.next();
-			writer.println(message);
-			writer.flush();
-			
-			//Der Versuch, alle Nachrichten in einer TXT-Datei zu speichern
-			File file = new File("qwert.txt");
-			BufferedWriter filewriter = new BufferedWriter(new FileWriter(file, true));
-			Iterator iterator = list_clientWriter.iterator();
-			while(iterator.hasNext()) {
-				PrintWriter filewriter2 = (PrintWriter) iterator.next();
-				filewriter.write(message);
-				filewriter.flush();
-			
-			}
+		Iterator<PrintWriter> it = list_clientWriter.iterator();
+
+		while (it.hasNext()) {
+			PrintWriter pw = (PrintWriter) it.next();
+			pw.println(message);
+			pw.flush();
+
 		}
-		
+
 	}
 	
+	public void saveChatHistory(String message) throws Exception {
+		// txt-file und Writer um den Chatverlauf zu speichern.
+		File file = new File("chatHistory.txt");
+		BufferedWriter fw = new BufferedWriter(new FileWriter(file, true));
+		
+		fw.write(message +"\n");
+		fw.flush();
+		fw.close();
+		
+	}
 
 }
